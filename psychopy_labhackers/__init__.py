be31c009bed92ba3e201d06d5895127e63ba78b5
mdcutone <matthew.cutone@gmail.com>
2022-12-07 15:15:32 -0500

    FF: Updated Sol's email

--- psychopy_labhackers/__init__.py
+++ psychopy_labhackers/__init__.py
@@ -5,6 +5,9 @@
 # Copyright (C) 2002-2018 Jonathan Peirce (C) 2019-2022 Open Science Tools Ltd.
 # Distributed under the terms of the GNU General Public License (GPL).
 
+"""Interfaces for LabHackers Research Equipment devices.
+"""
+
 __version__ = '0.0.1'
 
 from .labhackers import getDevices, getSerialPorts, getUSB2TTL8s, USB2TTL8
